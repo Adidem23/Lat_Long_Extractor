9c6357832f082452a8c04bdf2614d37f70aadb77
Adidem23 <adityasuryawanshi5451@gmail.com>
2024-08-07 02:32:41 +0530

    Feat:Added New print Line For State

--- Location.py
+++ Location.py
@@ -26,6 +26,7 @@ def ExtractLatiandLongitude():
 
             for i in JSON_Object:
                 if str.lower(i['area'])==str.lower(Area):
+                    print(f"Pincode Belongs to State {i['state']}")
                     print(f"Latitude and Logitude for {Area} are {i['lat']} and {i['lng']}")
 
 if __name__=="__main__":
